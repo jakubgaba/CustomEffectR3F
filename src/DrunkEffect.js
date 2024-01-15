4b35cd5f0fc223509dcb1783f49b5bb4605cdae5
Jakub <j.gaba@email.cz>
2024-01-15 21:11:49 +0100

    Done?

--- src/DrunkEffect.js
+++ src/DrunkEffect.js
@@ -3,10 +3,11 @@ import { Uniform } from "three";
 const fragmentShader = /*glsl*/`
 uniform float frequency;
 uniform float amplitude;
+uniform float offset;
 
 void mainUv(inout vec2 uv)
 {
- uv.y += sin(uv.x * frequency) * amplitude;
+ uv.y += sin(uv.x * frequency + offset) * amplitude;
 }
 
 void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
@@ -25,10 +26,14 @@ export default class DrunkEffect extends Effect {
                 blendFunction: blendFunction,
                 uniforms: new Map([
                     ['frequency', new Uniform(frequency)],
-                    ['amplitude', new Uniform(amplitude)]
+                    ['amplitude', new Uniform(amplitude)],
+                    ['offset', new Uniform(0)]
                 ])
             }
-            )
+        )
+    }
 
+    update(renderer, inputBuffer, deltaTime) {
+        this.uniforms.get('offset').value += deltaTime
     }
 }
